de237e7caaa4ac507491568b7d58c9c2930d40aa
Andy Szeto <szeto.sabo@gmail.com>
2020-03-16 09:01:36 -0700

    Create HowManyNumsSmallerThanCurrentNum.java

---
+++ HowManyNumsSmallerThanCurrentNum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        
+        Stack<Integer> s1 = new Stack<Integer>();
+        int[] auxArr = new int[nums.length];
+        
+        for(int i = nums.length-1; i >=0; i--){
+        
+            s1.push(nums[i]);    
+            
+        }
+        
+        int counter = 0;
+        while (!s1.empty()){
+            
+            int val = s1.peek();
+            
+            for(int i = 0; i < nums.length; i++){
+                
+                if(val > nums[i]){
+                    
+                    auxArr[counter]++;
+                    
+                }   
+                
+            }
+            counter++;
+            s1.pop();
+            }
+            
+        return auxArr;
+        
+    }
+}
