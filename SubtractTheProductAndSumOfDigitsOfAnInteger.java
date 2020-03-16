2339e602407bcf3997100887b0ed29b60c368320
Andy Szeto <szeto.sabo@gmail.com>
2020-03-16 09:03:20 -0700

    Create SubtractTheProductAndSumOfDigitsOfAnInteger.java

---
+++ SubtractTheProductAndSumOfDigitsOfAnInteger.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        
+        int product = 1;
+        int sum = 0;
+        
+        while(n > 0){
+            
+            int val = n%10;
+            product*=val;
+            sum+=val;
+            n/=10;
+        }
+        
+        int diff = product - sum;
+        return diff;
+    }
+}
