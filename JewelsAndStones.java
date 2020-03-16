5303006bf56e234f91afb7ed926c84d1200281ff
Andy Szeto <szeto.sabo@gmail.com>
2020-03-16 08:59:04 -0700

    Create JewelsAndStones.java

---
+++ JewelsAndStones.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        
+        Stack<Character> s1 = new Stack<Character>();
+        int jewelCount = 0;
+        
+        for(int i = 0; i< J.length(); i++){
+            
+            s1.push(J.charAt(i));
+                
+            }
+        
+        
+        while(!s1.isEmpty()) {
+        	for(int i = 0; i< S.length(); i++){
+            
+        	if(s1.peek() == S.charAt(i)) {
+        		
+        		jewelCount++;
+        		
+        	}
+            
+        	
+        	}
+        s1.pop();
+        }
+        
+        
+        
+        return jewelCount;
+    }
+}
