8f88208ee8f3c7031e002eb885e82c7de92a7e66
Andy Szeto <szeto.sabo@gmail.com>
2020-05-23 15:59:01 -0700

    Update HowManyNumsSmallerThanCurrentNum.java

--- HowManyNumsSmallerThanCurrentNum.java
+++ HowManyNumsSmallerThanCurrentNum.java
@@ -5,30 +5,22 @@ class Solution {
         int[] auxArr = new int[nums.length];
         
         for(int i = nums.length-1; i >=0; i--){
-        
-            s1.push(nums[i]);    
-            
+            s1.push(nums[i]);       
         }
         
         int counter = 0;
         while (!s1.empty()){
-            
             int val = s1.peek();
             
             for(int i = 0; i < nums.length; i++){
-                
                 if(val > nums[i]){
-                    
                     auxArr[counter]++;
-                    
                 }   
-                
             }
             counter++;
             s1.pop();
             }
             
         return auxArr;
-        
     }
 }
