d01fae6fcceb4e2f9ac6bb53ee96366073d16a0d
Andy Szeto <szeto.sabo@gmail.com>
2020-05-23 16:03:58 -0700

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -4,20 +4,13 @@ class Solution {
         int[] arr1 = new int[2];
         
         for(int i = 0; i < nums.length; i++){
-            
             for(int k = i+1; k < nums.length; k++){
-                
                 if(nums[i] + nums[k] == target){
-                    
                     arr1[0] = i;
                     arr1[1] = k;
-                }
-                
-            }
-            
-            
+                }   
+            }  
         }
-        
         return arr1;
     }
 }
