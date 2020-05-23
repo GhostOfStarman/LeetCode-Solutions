017c5aef7594711dbd9b4ee1aec16fc130083874
Andy Szeto <szeto.sabo@gmail.com>
2020-05-23 15:55:11 -0700

    Update CreateTargetArrayInTheGivenOrder.java

--- CreateTargetArrayInTheGivenOrder.java
+++ CreateTargetArrayInTheGivenOrder.java
@@ -5,19 +5,15 @@ class Solution {
 	        int[] target = new int[nums.length];
 	        
 	        for(int i = 0; i < nums.length; i++){
-	             
 	            int val = index[i];
 	            int val1 = nums[i];
 
 	            a1.add(val, val1);
-
 	        }
 	        
 	        for(int j = 0; j< nums.length; j++) {
-	        	
 	        	target[j] = a1.get(j);
 	        }
-
 	        return target;
 	    }
 }
