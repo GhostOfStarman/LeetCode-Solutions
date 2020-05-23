1d8b99f32874cea64833ccf6467e743c1ff88386
Andy Szeto <szeto.sabo@gmail.com>
2020-05-23 16:01:14 -0700

    Update NumberStepsToReduceANumberToZero.java

--- NumberStepsToReduceANumberToZero.java
+++ NumberStepsToReduceANumberToZero.java
@@ -4,22 +4,15 @@ class Solution {
         int count = 0;
         
         while(num>0){
-            
             if(num%2 == 0){
-                
                 num /= 2;
                 count++;
-                
             }
-            
             if(num%2 == 1){
-                
                 num-=1;
                 count++;
             }
-            
         }
-        
         return count;
     }
 }
