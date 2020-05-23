337d40c399238d65b0bae572e7e142090b7df49b
Andy Szeto <szeto.sabo@gmail.com>
2020-05-23 15:55:30 -0700

    Update DecompressRun-LengthEncodedList.java

--- DecompressRun-LengthEncodedList.java
+++ DecompressRun-LengthEncodedList.java
@@ -4,21 +4,17 @@ class Solution {
         LinkedList<Integer> l1 = new LinkedList<Integer>();
         
         for(int i = 0; i < nums.length; i+=2){
-            
             int val1 = nums[i];
             int val2 = nums[i+1];
             
             for(int j = 0; j< val1; j++) {
             	l1.add(val2);
-            }
-            
+            }   
         }
        
         int[] retArr = new int[l1.size()];
         for(int k = 0; k < retArr.length; k++) {
-        	
-        	retArr[k] = l1.get(k);
-        	
+        	retArr[k] = l1.get(k);	
         }
         
         return retArr;
