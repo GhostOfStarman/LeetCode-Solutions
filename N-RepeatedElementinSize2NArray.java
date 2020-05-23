a9d8c0b45433b7beaf9f4d597ee3f60779bc11d3
Andy Szeto <szeto.sabo@gmail.com>
2020-05-23 15:52:14 -0700

    Update N-RepeatedElementinSize2NArray.java

--- N-RepeatedElementinSize2NArray.java
+++ N-RepeatedElementinSize2NArray.java
@@ -4,33 +4,21 @@ class Solution {
         Map<Integer, Integer> m2 = new HashMap< Integer, Integer>();
         
         for(int i = 0; i< A.length; i++){
-        	
         	int holder = A[i];
-            
-            if(!(m2.containsKey(holder))){
-                
-                m2.put(holder, 1);
-                
-            }
-            
-            else{
-                
+            if(!(m2.containsKey(holder))){              
+                m2.put(holder, 1);                
+            }else{
                 int num = m2.get(holder);
                 num++;
-                m2.put(holder, num);
-                
+                m2.put(holder, num); 
             }
-            
         }
         
         for (Map.Entry<Integer,Integer> entry : m2.entrySet()) {
-        	
         	if(entry.getValue() > 1) {
         		return entry.getKey();
         	}
-        	
         }
-
         return 0;
     }
 }
